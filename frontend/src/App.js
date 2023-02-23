2d98717025bd9564183d1d97db20dce1fd17e920
Yuvraj Rodage <jayrajrodge2001@gmail.com>
2023-02-24 00:13:59 +0530

    error fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import abi from "./artifacts/Contracts/Lottery.sol/Lottery.json";
+import abi from "./Artifacts/Lottery.json";
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import Intro from "./component/Intro";
